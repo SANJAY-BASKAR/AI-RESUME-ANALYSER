0807fbe16900d6b2a7bb4741a98ef0cc256b846a
Sanjay Baskar <sanjaybaskar.in@gmail.com>
2025-07-21 23:13:56 +0530

    Done with the authentication in puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { isLoading, auth } = usePuterStore();
+  // trick to sign in user properly with next parameter
+  // use effect and functions are here for just authenticating redirection
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your Application & resume ratings</h1>
+        <h2>Review your submissions and get AI powered feedbacks.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume}/>
+            ))}
+          </div>
+      )}
+    </section>
+  </main>;
 }
