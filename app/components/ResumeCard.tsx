274762160e8b38816daf943948d4f6eefc4e28ce
Sanjay Baskar <sanjaybaskar.in@gmail.com>
2025-07-23 11:23:31 +0530

    Now i have created the value of fetching real resume from the puter js and got it through console

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import {Link} from "react-router";
+import React, {useEffect, useState} from 'react'
+import {Link, useNavigate} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import {usePuterStore} from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const [resumeUrl, setResumeUrl] = useState("");
+    const { auth, fs } = usePuterStore();
+    // trick to sign in user properly with next parameter
+    // use effect and functions are here for just authenticating redirection
+
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const loadResume = async  () => {
+            const blob = await fs.read(imagePath);
+            if (!blob) return;
+
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+        loadResume();
+
+    }, [imagePath]);
+
     return (
         <Link to={`/resume.${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    <h2 className="!text-black font-bold break-words">
+                    {companyName &&<h2 className="!text-black font-bold break-words">
                         {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-gray-500">
+                    </h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">
                         {jobTitle}
-                    </h3>
+                    </h3>}
+                    {!companyName && !jobTitle && (
+                        <div className="text-black font-bold">Resume</div>
+                    )}
                 </div>
                 <div className="flex-shrink-0">
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-            <div className="gradient-border animate-in fade-in duration-1000">
+            {resumeUrl && <div className="gradient-border animate-in fade-in duration-1000">
                     <div className="w-full h-full">
                         <img
                             src={imagePath}
@@ -26,7 +50,7 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                             className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                         />
                     </div>
-            </div>
+            </div>}
 
 
         </Link>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,43 +1,66 @@
-import type { Route } from "./+types/home";
-import Navbar from "~/components/Navbar";
-import {resumes} from "../../constants";
-import ResumeCard from "~/components/ResumeCard";
-import {usePuterStore} from "~/lib/puter";
-import {useLocation, useNavigate} from "react-router";
-import {useEffect} from "react";
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Resumind" },
-    { name: "description", content: "Smart feedback for dream job" },
-  ];
-}
+  import type { Route } from "./+types/home";
+  import Navbar from "~/components/Navbar";
+  import ResumeCard from "~/components/ResumeCard";
+  import {usePuterStore} from "~/lib/puter";
+  import {useNavigate} from "react-router";
+  import {useEffect, useState} from "react";
+  export function meta({}: Route.MetaArgs) {
+    return [
+      { title: "Resumind" },
+      { name: "description", content: "Smart feedback for dream job" },
+    ];
+  }
 
-export default function Home() {
-  const { isLoading, auth } = usePuterStore();
-  // trick to sign in user properly with next parameter
-  // use effect and functions are here for just authenticating redirection
+  export default function Home() {
+    const { auth, kv } = usePuterStore();
+    // trick to sign in user properly with next parameter
+    // use effect and functions are here for just authenticating redirection
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState('false');
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if(!auth.isAuthenticated) navigate('/auth?next=/');
-  }, [auth.isAuthenticated])
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading py-16">
-        <h1>Track your Application & resume ratings</h1>
-        <h2>Review your submissions and get AI powered feedbacks.</h2>
-      </div>
+      useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+      }, [auth.isAuthenticated])
 
-      {resumes.length > 0 && (
-          <div className="resumes-section">
-            {resumes.map((resume) => (
-                <ResumeCard key={resume.id} resume={resume}/>
-            ))}
-          </div>
-      )}
-    </section>
-  </main>;
-}
+    useEffect(() => {
+      const loadResumes = async () => {
+        setLoadingResumes(true);
+
+        const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+        const parsedResumes = resumes?.map((resume) => (
+            JSON.parse(resume.value) as Resume
+        ))
+
+        console.log(parsedResumes || []);
+
+        setResumes(parsedResumes || []);
+        setLoadingResumes(false);
+      }
+
+      loadResumes()
+    }, []);
+
+
+
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Application & resume ratings</h1>
+          <h2>Review your submissions and get AI powered feedbacks.</h2>
+        </div>
+
+        {!loadingResumes && resumes.length > 0 && (
+            <div className="resumes-section">
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume}/>
+              ))}
+            </div>
+        )}
+      </section>
+    </main>;
+  }
